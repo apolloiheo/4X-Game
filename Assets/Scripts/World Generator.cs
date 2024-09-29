7e466d6d231c03b29de3cbbc8652754eaa35093e
Sederic <sergioandresdelrio@gmail.com>
2024-09-29 15:51:48 -0700

    Starting to work on World gen

--- Assets/Scripts/World Generator.cs
+++ Assets/Scripts/World Generator.cs
@@ -26,12 +26,64 @@ public class WorldGenerator : MonoBehaviour
     /* Determine Continent Numbers */
     private void DetermineContinents(World world)
     {
+        int worldLength = world.GetLength();
+        int worldHeight = world.GetHeight();
         
+        if (world.GetContinents() == 1)
+        {
+            world.SetContinentPoint1(new Point(worldLength / 2, worldHeight / 2));
+            
+        } else if (world.GetContinents() == 2)
+        {
+            world.SetContinentPoint1(new Point(worldLength / 4, worldHeight / 2));
+            world.SetContinentPoint2(new Point((worldHeight / 4) * 3 , worldHeight / 2));
+        }
     }
 
-    /* Determine the area of Continents proportional to world size. */
+    /* Determine the area of Continents proportional to world size.
+        - Put basic Plains tiles around the ContinentStartPoints
+        - Randomly expand tiles around it to form continent. 
+        - Stop when it reaches a % of world coverage.
+     */
     private void DetermineLand(World world)
     {
+        int totalWorldSize = world.GetLength() * world.GetHeight();
+        int desiredWorldCoverage = totalWorldSize / 3; // WIP - How much relative space our continent should take relative to world size.
+        int currentWorldCoverage = 1; // How much relative space our continent is taking relative to world size.
+
+        Point continentStart1 = world.GetContinentPoint1();
+        Point continentStart2 = world.GetContinentPoint2();
+        Point continentStart3 = world.GetContinentPoint3();
+        
+        switch (world.GetContinents())
+        {
+            case 1: // One Continent
+                break;
+            case 2: // Two Continents
+                
+                
+                world.ModifyTileBiome(continentStart1, 1);
+                world.ModifyTileBiome(continentStart2, 1);
+                
+                while (currentWorldCoverage < desiredWorldCoverage)
+                {
+                    // Initial Convert neighbor's to Plains.
+                    foreach (Tile t in world.GetTile(continentStart1).GetNeighbors())
+                    {
+                        Point neighborLocation = new Point(t.GetXPos(), t.GetYPos());
+                        world.ModifyTileBiome(neighborLocation, 1);
+                        currentWorldCoverage++;
+                    }
+                    
+                    
+                }
+                
+                break;
+            case 3: // Three Continents
+                break;
+        }
+        
+        
         
     }
 
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -8,8 +8,11 @@ public class World : MonoBehaviour
     // Instance Attributes
     private int _length;
     private int _height;
-    private Tile[,] _world; // 2D Array of Tiles
     private int _continents; // # of Continents in our world. Could be 1, 2 or 3.
+    Point _continentPoint1;
+    Point _continentPoint2;
+    Point _continentPoint3;
+    private Tile[,] _world; // 2D Array of Tiles
     
     // Constants
     private const int DefaultBiomeFill = 7; // 1.Plains, 2.Grassland, 3.Tundra, 4.Desert, 5.Snow, 6.Coast, 7.Ocean
@@ -134,4 +137,94 @@ public class World : MonoBehaviour
         
         Debug.Log(output);
     }
+    
+    // Tile Modification Methods
+    public void ModifyTileBiome(Point point, int biome)
+    {
+        _world[point.x, point.y].SetBiome(biome); 
+    }
+
+    public void ModifyTileTerrain(Point point, int terrain)
+    {
+        _world[point.x, point.y].SetTerrain(terrain);
+    }
+
+    public void ModifyTileFeature(Point point, int feature)
+    {
+        _world[point.x, point.y].SetFeature(feature);
+    }
+
+    public void ModifyTileResource(Point point, int resource)
+    {
+        _world[point.x, point.y].SetResource(resource);
+    }
+    
+    
+    
+    // Getter Methods
+
+    public int GetLength()
+    {
+        return _length;
+    }
+
+    public int GetHeight()
+    {
+        return _height;
+    }
+
+    public int GetContinents()
+    {
+        return _continents;
+    }
+
+    public Point GetContinentPoint1()
+    {
+        return _continentPoint1;
+    }
+
+    public Point GetContinentPoint2()
+    {
+        return _continentPoint2;
+    }
+
+    public Point GetContinentPoint3()
+    {
+        return _continentPoint3;
+    }
+
+    public Tile[,] GetWorld()
+    {
+        return _world;
+    }
+
+    public Tile GetTile(int x, int y)
+    {
+        return _world[x, y];
+    }
+
+    public Tile GetTile(Point point)
+    {
+        return _world[point.x, point.y];
+    }
+    
+    // Setter Methods
+
+    public void SetContinentPoint1(Point point)
+    {
+        _continentPoint1 = point;
+    }
+
+    public void SetContinentPoint2(Point point)
+    {
+        _continentPoint2 = point;
+    }
+
+    public void SetContinentPoint3(Point point)
+    {
+        _continentPoint3 = point;
+    }
+    
+    
+    
 }

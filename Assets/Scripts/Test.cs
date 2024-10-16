a3b65634d6f118d95b5e7b82d6a630f7a697f17b
Sederic <sergioandresdelrio@gmail.com>
2024-10-16 14:58:50 -0700

    Removed freshwater testing code

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -202,8 +202,9 @@ public class Test : MonoBehaviour
 
                 if (currTile.GetFreshWaterAccess())
                 {
-                    tilemap.SetTile(new Vector3Int(y, x, 0), tile);
-                    tile.color = Color.white;
+                    // For testing FreshWaterAccess
+                    /*tilemap.SetTile(new Vector3Int(y, x, 0), tile);
+                    tile.color = Color.white;*/
                     
                     for (int index = 0; index < 6; index++)
                     {
